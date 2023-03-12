625311bd8a903d39f78638d48cdae4fc694cfaa4
kur03 <cmaillot@et.esiea.fr>
2023-03-12 11:46:44 +0100

    change main - test kernel + function

--- Casseur_de_mdp/gpu.py
+++ Casseur_de_mdp/gpu.py
@@ -1,13 +1,22 @@
 from numba import cuda
-from brute_force import combinaisons
+import hashlib
 
-@cuda.jit 
-def kernel_combinaisons(mdp, type_hash):
+@cuda.jit
+def kernel_combinaisons(mdp, hash):
     all = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','@','[',']','^','_','!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','{','}','<','>','=','|','~','?']
-  
+    # tx = cuda.threadIdx.x
+    # bx = cuda.blockIdx.x
+    # bw = cuda.blockDim.x
+    # i = tx + bx * bw
     #taille maximale que fait notre mdp
     MAX_LENGTH = 15
     
-    for i in range(MAX_LENGTH): #Les différentes tailles de mdp à trouver
-        combinaisons(mdp, 0, i, '', all, type_hash)
-        print("Tous les mots de passe de", i, "caractères ont été testé.")
+    #if i <MAX_LENGTH: #Les différentes tailles de mdp à trouver
+    for y in range (0,MAX_LENGTH) : 
+        for i in range(len(all)):
+            mdp += all[i]
+            if(hashlib.sha1(mdp.encode()).hexdigest() == hash) :
+                print("Le mot de passe est : ", mdp)
+                return mdp
+        
+        #kernel_combinaisons( mdp,  hash) #Recursion avec caractère suivant)
--- Casseur_de_mdp/main.py
+++ Casseur_de_mdp/main.py
@@ -1,48 +1,30 @@
-from dictionnaire import dico
+import hashlib
 from brute_force import bf
 import gpu
-
-open('motnombre.txt', 'w').close()
-open('perso1.txt', 'w').close()
-open('test.txt', 'w').close()
-
-answer = input(
-    "Bonjour !\nConnaissez-vous quel algorithme a été utilisé ?\n\t1. Oui\n\t2. Non\n\nVeuillez entrer le chiffre correspondant. "
-)
-
-if answer == '1':
-	HASH = ''
-	TYPE_HASH = input(
-	    "\nVeuillez choisir\n\t1. Sha1\n\t2. MD5\n\t3. Autre\n\nVeuillez entrer le chiffre correspondant. "
-	)
-
-	if TYPE_HASH == '1':
-		HASH = 'sha1'
-
-	elif TYPE_HASH == '2':
-		HASH = 'MD5'
-
-	else:
-		print(
-		    "\nDésolé mais nous ne pouvons pas casser votre mdp pour le moment."
-		)
+from numba import cuda
+import numpy
+import time
+# import tensorflow as tf
 
 
+HASH = 'a9993e364706816aba3e25717850c26c9cd0d89d'
+mdp = numpy.empty(shape=0)
+all = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','@','[',']','^','_','!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','{','}','<','>','=','|','~','?']
+start = time.perf_counter()
+bf('sha1', HASH)
+end = time.perf_counter()
+print("elapsed time before cuda implementation = {}s".format((end - start)))
 
-mdp = input(
-	    "\nVeuillez entrer votre hash. "
-	)  #il ne faut pas mettre un hash pour l'instant car version test juste mettre des mots ou lettres random
-
-Mode = input(
-	    "\nQuelle méthode voulez-vous utiliser ?\n\t1. Brute force\n\t2. Dictionnaire\n\nVeuillez entrer le chiffre correspondant. "
-	)
-
-if Mode == '1' :
-    	gpu.kernel_combinaisons[5,5](mdp, HASH)
-		#bf(HASH, mdp)
-
-elif Mode == '2':
-    dico(HASH, mdp)
-    
-else:
-	  print("Veuillez choisir parmis les chiffres proposés.")
+d_HASH = cuda.to_device(HASH)
+d_mdp = cuda.to_device(mdp)
+start = time.perf_counter()
+gpu.kernel_combinaisons[5,2](d_mdp, d_HASH)
+cuda.syncthreads()
+end = time.perf_counter()
+mdp = numpy.empty(shape=d_mdp.shape, dtype=d_mdp.dtype)
+d_mdp.copy_to_host(mdp)
+HASH = numpy.empty(shape=d_HASH.shape, dtype=d_HASH.dtype)
+d_HASH.copy_to_host(HASH)
+print(mdp)
+print(HASH)
+print("elapsed time before cuda implementation= {}s".format((end - start)))

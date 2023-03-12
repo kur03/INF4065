c040ebd66ed75148ff75b97a6e8ddd65aeb9bb13
kur03 <cmaillot@et.esiea.fr>
2023-03-12 13:23:10 +0100

    correction mdp in gpu.py

--- Casseur_de_mdp/main.py
+++ Casseur_de_mdp/main.py
@@ -8,7 +8,6 @@ import time
 
 
 HASH = 'a9993e364706816aba3e25717850c26c9cd0d89d'
-mdp = numpy.empty(shape=0)
 all = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','@','[',']','^','_','!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','{','}','<','>','=','|','~','?']
 start = time.perf_counter()
 bf(HASH)
@@ -18,7 +17,7 @@ print("elapsed time before cuda implementation = {}s".format((end - start)))
 d_HASH = cuda.to_device(HASH)
 d_mdp = cuda.to_device(mdp)
 start = time.perf_counter()
-gpu.kernel_combinaisons[5,2](d_mdp, d_HASH)
+gpu.kernel_combinaisons[5,2](d_HASH)
 cuda.syncthreads()
 end = time.perf_counter()
 mdp = numpy.empty(shape=d_mdp.shape, dtype=d_mdp.dtype)

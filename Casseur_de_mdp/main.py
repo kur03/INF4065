6fd7453da7e5d905556d10cf1a9884669c91fb11
kur03 <cmaillot@et.esiea.fr>
2023-03-12 13:16:41 +0100

    brute force itérative

--- Casseur_de_mdp/main.py
+++ Casseur_de_mdp/main.py
@@ -11,8 +11,7 @@ HASH = 'c41975d1dae1cc69b16ad8892b8c77164e84ca39'
 mdp = numpy.empty(shape=0)
 all = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','@','[',']','^','_','!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','{','}','<','>','=','|','~','?']
 start = time.perf_counter()
-mdp = bf('sha1', HASH)
-print(mdp)
+bf(HASH)
 end = time.perf_counter()
 print("elapsed time before cuda implementation = {}s".format((end - start)))
 
